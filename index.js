6a49afaf83b2d36d768abe7dca87065689de9c70
Alhasan Almatwari <alhasaniscoding@outlook.com>
2023-07-28 01:14:27 -0400

    Added many changes.
    -- Created a index.js file and connected it to the app.
    -- Index.js has a function to call on weather api.
    -- Added more weather-icons.
    -- installed dotenv.
    -- updated .gitignore to include .env files.
    -- small tweaks to CSS and HTML.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-placer Text
+
+const apiKey = "e68e411f74abcb867789e5d80703f632";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    try {
+        const response = await fetch(apiUrl + city + '&appid=' + apiKey);
+        var data = await response.json(); 
+
+        console.log(data);
+
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+searchBtn.addEventListener("click", async ()=>{
+
+   await checkWeather(searchBox.value);
+
+});
 
 
